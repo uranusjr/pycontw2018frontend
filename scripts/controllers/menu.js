2ed66a97d63ac6c6a51b73bc39eb2938c84b5cc8
Tzu-ping Chung <uranusjr@gmail.com>
2018-02-18 04:42:58 +0800

    Comments

--- scripts/controllers/menu.js
+++ scripts/controllers/menu.js
@@ -5,10 +5,12 @@ export class MenuController extends Controller {
 	static targets = ['item']
 
 	connect() {
+		// Click anywhere on the page to close the menu.
 		document.documentElement.addEventListener('click', e => this.exclude(e))
 	}
 
 	exclude(event) {
+		// Close all menus except the one triggering this event.
 		this.itemTargets.forEach(target => {
 			if (target !== event.target && target.checked) {
 				target.checked = false
