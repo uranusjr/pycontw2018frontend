d5aa1f04bf8ed965313aa8f3c2ab457a122d1954
Tzu-ping Chung <uranusjr@gmail.com>
2018-02-18 04:41:31 +0800

    Click anywhere else to close menu

--- scripts/controllers/menu.js
+++ scripts/controllers/menu.js
@@ -4,6 +4,10 @@ export class MenuController extends Controller {
 
 	static targets = ['item']
 
+	connect() {
+		document.documentElement.addEventListener('click', e => this.exclude(e))
+	}
+
 	exclude(event) {
 		this.itemTargets.forEach(target => {
 			if (target !== event.target && target.checked) {
