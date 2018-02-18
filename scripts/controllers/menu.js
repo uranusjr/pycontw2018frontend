e415af0a9ba2ffa0f5c5b5828a910915f28ab256
Tzu-ping Chung <uranusjr@gmail.com>
2018-02-18 21:19:55 +0800

    Fix bug where a menu item can't uncheck itself
    Honestly I'm not 100% sure the comment is correct. It seems to be, and
    this workaround functions correctly, but I did not actually reason it
    comprehensively. I don't really care, truth be told.

--- scripts/controllers/menu.js
+++ scripts/controllers/menu.js
@@ -6,7 +6,14 @@ export class MenuController extends Controller {
 
 	connect() {
 		// Click anywhere on the page to close the menu.
-		document.documentElement.addEventListener('click', e => this.exclude(e))
+		document.documentElement.addEventListener('click', event => {
+			// Don't run if the event originates from the menu, to avoid triggering
+			// the handler twice when clicking on a menu item. Otherwise a menu item
+			// will not be able to uncheck itself.
+			if (!self.itemTargets.includes(event.target)) {
+				this.exclude(event)
+			}
+		})
 	}
 
 	exclude(event) {
